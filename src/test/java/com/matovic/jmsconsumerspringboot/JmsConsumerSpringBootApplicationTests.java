ec7d744e60b2792096514ac9d4f7e0bbeffead69
aleksamatovic <lexamatovic@gmail.com>
2022-08-20 22:36:32 +0200

    Publisher and consumer services

---
+++ src/test/java/com/matovic/jmsconsumerspringboot/JmsConsumerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matovic.jmsconsumerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsConsumerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
